953b83e699af559d024c0659bdda2bace24a0dc8
Jean <jeanpolo15@gmail.com>
2023-07-27 11:31:44 -0500

    ajustes en el formulario

--- script.js
+++ script.js
@@ -21,6 +21,13 @@ const mensajesDeError = {
     }
 }
 
+const campos = {
+    nombre: false,
+    email: false,
+    asunto: false,
+    mensaje: false
+}
+
 const validaFormulario = (e) => {
     switch (e.target.name) {
     case "nombre":
@@ -45,9 +52,11 @@ const validarCampo = (expresion, input, campo) => {
     if (expresion.test(input.value)) {
         document.getElementById(`grupo__${campo}`).classList.remove('form__grupo-incorrecto');
         document.querySelector(`#grupo__${campo} .form__input-error`).classList.remove('form__input-error-activo');
+        campos[campo] = true;
     } else {
         document.getElementById(`grupo__${campo}`).classList.add('form__grupo-incorrecto');
         document.querySelector(`#grupo__${campo} .form__input-error`).classList.add('form__input-error-activo');
+        campos[campo] = false;
     }
 }
 
@@ -58,4 +67,13 @@ inputs.forEach((input) => {
 
 formulario.addEventListener('submit', (e) => {
     e.preventDefault();
+
+    if(campos.nombre && campos.email && campos.asunto && campos.mensaje) {
+        formulario.reset();
+
+        document.getElementById('form__mensaje-exito').classList.add('form__mensaje-exito-activo');
+        setTimeout(() => {
+            document.getElementById('form__mensaje-exito').classList.remove('form__mensaje-exito-activo');
+        }, 5000);
+    }
 });
